5c261480fffea2cf06ed054b28bc3d3c070f7da6
2-double-G <farewak2@gmail.com>
2020-12-14 00:39:04 +0300

    Search elements in DB

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -8,12 +8,15 @@ import { directorsQuery } from './queries';
 
 import { styles } from './styles';
 
-const withGraphql = compose(
+const withGraphQL = compose(
   graphql(addMovieMutation, {
     props: ({ mutate }) => ({
       addMovie: movie => mutate({
         variables: movie,
-        refetchQueries: [{ query: moviesQuery }],
+        refetchQueries: [{
+          query: moviesQuery,
+          variables: { name: '' },
+        }],
       }),
     }),
   }),
@@ -21,10 +24,18 @@ const withGraphql = compose(
     props: ({ mutate }) => ({
       updateMovie: movie => mutate({
         variables: movie,
-        refetchQueries: [{ query: moviesQuery }],
+        refetchQueries: [{
+          query: moviesQuery,
+          variables: { name: '' },
+        }],
       }),
     }),
-  })
-)
+  }),
+  graphql(directorsQuery, {
+    options: ({ name = '' }) => ({
+      variables: { name },
+    }),
+  }),
+);
 
-export default compose(withStyles(styles), graphql(directorsQuery), withGraphql);
+export default compose(withStyles(styles), withGraphQL);
