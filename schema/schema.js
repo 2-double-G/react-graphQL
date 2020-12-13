07675afa0bf80c8267015dcd0534e42f7b449172
2-double-G <farewak2@gmail.com>
2020-12-13 18:01:25 +0300

    Adding Mongoose models & updating resolve

--- schema/schema.js
+++ schema/schema.js
@@ -1,27 +1,19 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
 
-// const movies = [
-// 	{ id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
-// 	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
-// 	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
-// 	{ id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
-// 	{ id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-// 	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-// 	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-// 	{ id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
-// ];
-
-// const directors = [
-// 	{ id: '1', name: 'Quentin Tarantino', age: 55 },
-// 	{ id: '2', name: 'Michael Radford', age: 72 },
-// 	{ id: '3', name: 'James McTeigue', age: 51 },
-// 	{ id: '4', name: 'Guy Ritchie', age: 50 },
-// ];
+const Movies = require("../models/movie");
+const Directors = require("../models/director");
 
 const MovieType = new GraphQLObjectType({
-  name: 'Movie',
+  name: "Movie",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -29,14 +21,13 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        // return directors.find(director => director.id == parent.id);
-      }
-    }
+        return Directors.findById(parent.directorId);      },
+    },
   }),
 });
 
 const DirectorType = new GraphQLObjectType({
-  name: 'Director',
+  name: "Director",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -44,63 +35,44 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        // return movies.filter(movie => movie.directorId === parent.id)
+        return Movies.find({ directorId: parent.id });
       },
     },
   }),
 });
 
 const Query = new GraphQLObjectType({
-  name: 'Query',
+  name: "Query",
   fields: {
     movie: {
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // return movies.find(movie => movie.id == args.id);
-      },      
+        return Movies.findById(args.id);
+      },
     },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // return directors.find(director => director.id == args.id);
-      },      
+        return Directors.findById(args.id);
+      },
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        // return movies;
+        return Movies.find({});
       },
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        // return directors;
-      }
-    }
-
+        return Directors.find({});
+      },
+    },
   },
 });
 
 module.exports = new GraphQLSchema({
   query: Query,
 });
-
-// const directorsJson = [
-//   { "name": "Quentin Tarantino", "age": 55 }, // 5fd5f88c685b5f7d2de35905
-//   { "name": "Michael Radford", "age": 72 }, // 5fd5f944685b5f7d2de35906
-//   { "name": "James McTeigue", "age": 51 }, // 5fd5fa19685b5f7d2de35907
-//   { "name": "Guy Ritchie", "age": 50 }, // 5fd5fa4a685b5f7d2de35908
-// ];
-
-// const moviesJson = [
-//   { "name": "Pulp Fiction", "genre": "Crime", "directorId": "5fd5f88c685b5f7d2de35905" },
-//   { "name": "1984", "genre": "Sci-Fi", "directorId": "5fd5f944685b5f7d2de35906" },
-//   { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "5fd5fa19685b5f7d2de35907" },
-//   { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "5fd5fa4a685b5f7d2de35908" },
-//   { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "5fd5f88c685b5f7d2de35905" },
-//   { "name": "The Hateful Eight", "genre": "Crime", "directorId": "5fd5f88c685b5f7d2de35905" },
-//   { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "5fd5f88c685b5f7d2de35905" },
-//   { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "5fd5fa4a685b5f7d2de35908" },
-// ];
--- server/app.js
+++ server/app.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user:user@graphql.advo7.mongodb.net/user?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect("mongodb+srv://user:user@graphql.advo7.mongodb.net/react-graphql?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }))
 
+const dbConnection = mongoose.connection;
+dbConnection.on('error', error => console.log(`Connection error: ${error}`));
+dbConnection.once('open', () => console.log('Connected to DB!'))
+
 app.listen(PORT, error => {
   error ? console.log(error) : console.log('Server started!');
 })
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user:user@graphql.advo7.mongodb.net/user?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("react-graphql").collection("devices");
+//   const dbConnection = mongoose.connection;
+//   // perform actions on the collection object
+//   client.close();
+// });
